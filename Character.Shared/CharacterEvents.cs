2e020bb9c9a15e6a00829c341f7a0efb51d0795d
Christopher Hayward <chayward1@athabasca.edu>
2019-12-17 23:22:30 -0700

    Remove reference to inventory (seperate module)

--- Character.Shared/CharacterEvents.cs
+++ Character.Shared/CharacterEvents.cs
@@ -13,7 +13,6 @@ namespace StreetlifeRP.Character.Shared
         public const string DeselectAll = "streetliferp:character:deselectall";
         public const string SaveCharacter = "streetliferp:character:save:character";
         public const string SavePosition = "streetliferp:character:save:position";
-        public const string GetCharacterInventories = "streetliferp:character:inventories";
 
         public const string Selecting = "streetliferp:character:selecting";
         public const string Selected = "streetliferp:character:selected";
